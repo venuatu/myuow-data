83335cdffc73c77a2b2853cac2baadf6151163f4
venuatu <venuatu@gmail.com>
2014-06-18 14:52:54 +1000

    Subject: add some cache headers

--- app/controllers/Subject.scala
+++ app/controllers/Subject.scala
@@ -5,9 +5,9 @@ import play.api.mvc._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.async.Async.{async, await}
 import helpers.{JsonHelpers, Extractors}
+import scala.concurrent.duration._
 import play.api.libs.json._
 import play.api.cache._
-import java.util.Calendar
 import scala.util.{Success, Failure}
 import models.{Description, DescriptionSearch, Timetable, TimetableSearch}
 import play.api.http.ContentTypes
@@ -15,13 +15,14 @@ import org.joda.time.LocalDate
 
 object Subject extends Controller with JsonHelpers {
   implicit val http = models.http
+  def CACHE_HEADER = (CACHE_CONTROL, "public, max-age="+ 1.day.toSeconds)
 
   def searchTimetables(code: String, year: Int, campus: Int) = Action.async {implicit request =>
     val realYear = if (year != 0) year else LocalDate.now().getYear
     TimetableSearch(code, realYear, campus).map {items =>
       Ok(jsonPretty(items.map{item =>
         jsonWithUrl(item, routes.Subject.getTimetable(item.id).absoluteURL())
-      })).as(ContentTypes.JSON)
+      })).as(ContentTypes.JSON).withHeaders(CACHE_HEADER)
     }
   }
 
@@ -30,20 +31,20 @@ object Subject extends Controller with JsonHelpers {
     DescriptionSearch(code, realYear).map{descriptions =>
       Ok(jsonPretty(descriptions.map{desc =>
         jsonWithUrl(desc, routes.Subject.getDescription(desc.id, desc.code, desc.year).absoluteURL())
-      })).as(ContentTypes.JSON)
+      })).as(ContentTypes.JSON).withHeaders(CACHE_HEADER)
     }
   }
 
   def getTimetable(id: Int) = Action.async {
     Timetable(id).map{subject =>
-      Ok(jsonPretty(subject)).as(ContentTypes.JSON)
+      Ok(jsonPretty(subject)).as(ContentTypes.JSON).withHeaders(CACHE_HEADER)
     }
   }
 
   def getDescription(id: Int, code: String, year: Int) = Action.async {
     val realYear = if (year != 0) year else LocalDate.now().getYear
     Description(id, code, realYear).map{subject =>
-      Ok(jsonPretty(subject)).as(ContentTypes.JSON)
+      Ok(jsonPretty(subject)).as(ContentTypes.JSON).withHeaders(CACHE_HEADER)
     }
   }
 
